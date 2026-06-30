bf492796f984be4a4f150d9a496ee5d26dcb2161
torika <imzenya5266@gmail.com>
2026-06-30 20:38:29 +0700

    Splash_Edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(SplashScreen());
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen()));
 }
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.green.shade300, Colors.green.shade700],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(child: Image.asset('assets/images/logo.png', width: 180,)),
+      ),
+    );
   }
 }
